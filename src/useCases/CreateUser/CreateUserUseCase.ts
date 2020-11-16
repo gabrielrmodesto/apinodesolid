93e6895b7af37f028d252d4253ebf3d08eb73c13
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-11-16 00:50:53 -0300

    Single responsability implement

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,19 @@
+import { User } from './../../entities/User';
+import { IUsersRepository } from './../../repositories/IUsersRepository';
+import { ICreateUserDTO } from './CreateUserDTO';
 export class CreateUserUseCase{
-	async execute(){
+	constructor(
+		private userRepository: IUsersRepository
+	){}
+	async execute(data: ICreateUserDTO){
+		const userAlreadyExists =  this.userRepository.findByEmail(data.email);
 
+		if(userAlreadyExists){
+			throw new Error('User already exists.');
+		}
+
+		const user = new User(data);
+
+		await this.userRepository.save(user);
 	}
 }
